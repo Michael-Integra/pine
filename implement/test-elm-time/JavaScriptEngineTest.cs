3c982a988b9a48a9ea77dd7a3aa7a25b5e4c3f02
Michael Rätzel <viir@viir.de>
2023-05-26 12:35:49 +0000

    Adapt Evaluate_in_JavaScriptEngine

--- implement/test-elm-time/JavaScriptEngineTest.cs
+++ implement/test-elm-time/JavaScriptEngineTest.cs
@@ -9,7 +9,7 @@ public class JavaScriptEngineTest
     [TestMethod]
     public void Evaluate_in_JavaScriptEngine()
     {
-        var jsEngine = JsEngineFromJavaScriptEngineSwitcher.ConstructJsEngine();
+        var jsEngine = IJsEngine.BuildJsEngine();
 
         Assert.AreEqual(4, jsEngine.Evaluate("3 + 1"));
     }
